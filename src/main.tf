c4d72bd29de8b07d87faa72097488a6f50202e70
Michael Kennedy <merpso@gmail.com>
2021-04-30 13:07:46 +1000

    big-iq addition to src main.tf

--- src/main.tf
+++ src/main.tf
@@ -4,7 +4,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-    availability_zones = data.aws_availability_zones.available.names
+  availability_zones = data.aws_availability_zones.available.names
 }
 
 # Initialise Random variable
@@ -12,21 +12,37 @@ resource "random_id" "id" {
   byte_length = 2
 }
 
+# Create random password for BIG-IP
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+# Create Secret Store and Store BIG-IP Password
+resource "aws_secretsmanager_secret" "bigiq" {
+  name = format("%s-secret-%s", var.tags.prefix, random_id.id.hex)
+}
+resource "aws_secretsmanager_secret_version" "bigiq-pwd" {
+  secret_id     = aws_secretsmanager_secret.bigiq.id
+  secret_string = random_password.password.result
+}
+
 # Create Module using community
 module "aws_vpc" {
-  description = "aws thing"
+  description          = "aws thing"
   source               = "terraform-aws-modules/vpc/aws"
   name                 = format("%s-%s-%s", var.tags.prefix, var.tags.environment, random_id.id.hex)
   cidr                 = var.aws_vpc_parameters.cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  azs   = local.availability_zones
+  azs = local.availability_zones
 
   # vpc public subnet used for external interface
   public_subnets = [
     for num in range(length(local.availability_zones)) :
-      cidrsubnet(var.aws_vpc_parameters.cidr, 8, num + var.cidr_offsets.external)
+    cidrsubnet(var.aws_vpc_parameters.cidr, 8, num + var.cidr_offsets.external)
   ]
   public_subnet_tags = {
     Name        = format("%s-%s-public", var.tags.prefix, random_id.id.hex)
@@ -82,4 +98,17 @@ module "aws_vpc" {
     Terraform   = "true"
     Environment = var.tags.environment
   }
+}
+
+module "big-iq" {
+  source = "github.com/merps/terraform-aws-bigiq"
+  admin_password = ""
+  aws_secretmanager_secret_id = ""
+  cm_license_keys = []
+  dcd_license_keys = []
+  ec2_key_name = ""
+  mgmt_subnet_security_group_ids = []
+  private_subnet_security_group_ids = []
+  vpc_mgmt_subnet_ids = []
+  vpc_private_subnet_ids = []
 }
